27162535026cba71ab1cd4ef57b9e847ef82b6ea
Dhruvesh <vaghasiyadhruvesh@gmail.com>
2025-05-16 18:21:30 +0530

    Update main files

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,11 @@
 import Demo from './demo.jsx';
 function App() {
+  const username = "dhruvesh"
   return (
     // if you want to render multiple element on the page then used <> </>
     <>
-      <h1>Hello world</h1>
+      {/* we can also attached variable in the html  */}
+      <h1>Hello world {username}</h1>
       <Demo />
     </> // It's called fragment
   );
--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,8 +1,28 @@
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).
-render(
-    <App />
+function MyApp() {
+    return (
+        <>
+            <h1>Custom App</h1>
+        </>
+    )
+}
 
+const anotherUser = "dhruv"
+
+const reactElement = React.createElement(
+    'a',
+    { href: 'https://google.com', target: '_blank' },
+    'Click me   ',
+    anotherUser     // we can also attached variable here
 )
+
+createRoot(document.getElementById('root')).
+    render(
+        // <MyApp />
+        reactElement   // this element gives output
+        // <App />
+        // MyApp() // We can also write like this
+    )
