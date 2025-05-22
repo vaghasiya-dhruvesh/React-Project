0cc0aa33824d277cdc65b77e0d40027254bf7349
Dhruvesh <vaghasiyadhruvesh@gmail.com>
2025-05-22 09:31:55 +0530

    Build Project

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,12 +1,54 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [password, setPassword] = useState("D#ruve$h");
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (numberAllowed) str += "0123456789"
+
+    if (charAllowed) str += "!@#$%^&*()~-+_{}[]"
+
+    for (let i = 1; i < length; i++)
+    {
+      let char = Math.floor(Math.random() * str.length + 1)
+      pass = str.charAt(char)
+    }
+
+    setPassword(pass)
+
+  }, [length, numberAllowed, charAllowed, setPassword])
 
   return (
     <>
-      <h1>Password Generator</h1>
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'>
+        <h1 className='text-white text-center m-3'>Password Generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3 bg-white'
+            placeholder='password'
+            readOnly
+          />
+          <button className='outline-none shrink py-0.5 px-3 text-white bg-blue-700'>Copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input type="range" min={6} max={100} value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label htmlFor="">Length: {length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'></div>
+        </div>
+      </div>
     </>
   )
 }
