de461a80318d1cd4f1b343b8e1700dbf8c5f073b
Dhruvesh <vaghasiyadhruvesh@gmail.com>
2025-05-31 09:48:50 +0530

    Build theme Toggler

---
+++ 09themeToggler/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+    ],
+    darkMode: "class", // or 'media' if you prefer automatic based on system
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
